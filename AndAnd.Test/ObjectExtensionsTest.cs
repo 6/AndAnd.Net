87b0809a5273bd78b051c1854eb8735b61e667d6
Peter Graham <peter@1000memories.com>
2013-11-05 11:17:11 -0800

    Initial commit

---
+++ AndAnd.Test/ObjectExtensionsTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+namespace AndAnd.Test
+{
+    [TestFixture]
+    public class ObjectExtensionsTest
+    {
+        public interface ITestInterface
+        {
+            string TestMethod1();
+            string TestMethod2();
+        }
+
+        public class TestClass : ITestInterface
+        {
+            public virtual string TestMethod1()
+            {
+                return null;
+            }
+
+            public virtual string TestMethod2()
+            {
+                return "non-null string";
+            }
+        }
+
+        [Test]
+        public void NullInterface()
+        {
+            ITestInterface testInterface = null;
+            Assert.That(testInterface.AndAnd().TestMethod1(), Is.Null);
+            Assert.That(testInterface.AndAnd().TestMethod2(), Is.Null);
+        }
+
+        [Test]
+        public void NonNullInterface()
+        {
+            ITestInterface testInterface = new TestClass();
+            Assert.That(testInterface.AndAnd().TestMethod1(), Is.Null);
+            Assert.That(testInterface.AndAnd().TestMethod2(), Is.EqualTo("non-null string"));
+        }
+
+        [Test]
+        public void NullClass()
+        {
+            TestClass testClass = null;
+            Assert.That(testClass.AndAnd().TestMethod1(), Is.Null);
+            Assert.That(testClass.AndAnd().TestMethod2(), Is.Null);
+        }
+
+        [Test]
+        public void NonNullClass()
+        {
+            TestClass testClass = new TestClass();
+            Assert.That(testClass.AndAnd().TestMethod1(), Is.Null);
+            Assert.That(testClass.AndAnd().TestMethod2(), Is.EqualTo("non-null string"));
+        }
+    }
+}
---
+++ AndAnd/ObjectExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Castle.DynamicProxy;
+
+namespace AndAnd
+{
+    public static class ObjectExtensions
+    {
+        private static readonly ProxyGenerator proxyGenerator = new ProxyGenerator();
+
+        public static T AndAnd<T>(this T obj) where T : class
+        {
+            if (obj == null)
+            {
+                if (typeof (T).IsInterface)
+                {
+                    return proxyGenerator.CreateInterfaceProxyWithoutTarget<T>(new ParameterInterceptor());
+                }
+                return proxyGenerator.CreateClassProxy<T>(new ParameterInterceptor());
+            }
+            return obj;
+        }
+    }
+
+    public class ParameterInterceptor : IInterceptor
+    {
+        public void Intercept(IInvocation invocation)
+        {
+            invocation.ReturnValue = null;
+        }
+    }
+}
